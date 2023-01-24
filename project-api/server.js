78ebdf008c8ae3d96264b2decb945ba838b42655
Tien Thanh <tienthanhhn@Thanhs-MacBook.local>
2023-01-24 16:34:53 +0700

    Add new commit

---
+++ project-api/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/UserRoute");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect("mongodb://localhost:27017/netflix",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(()=>{
+    console.log("DB connected");
+});
+
+app.use("/api/user", userRoutes);
+
+app.listen(5000,console.log("server started"));
